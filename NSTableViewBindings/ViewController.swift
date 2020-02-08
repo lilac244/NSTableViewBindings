a8af483bf9f5450d2276011ece4bafee6b764f87
lilac244 <sola.ray.lino@gmail.com>
2020-02-08 11:58:55 +0900

    Initial Commit

---
+++ NSTableViewBindings/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NSTableViewBindings
+//
+//  Created by tsuyoshi on 2020/02/08.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
